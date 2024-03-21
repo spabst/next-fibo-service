5b084add7beea8275c0cde240451a59123335697
Sebastian Pabst <sebapabst@gmail.com>
2024-03-22 00:19:37 +0100

    formatting fibonacci function file

--- test/test_fibonacci_function.py
+++ test/test_fibonacci_function.py
@@ -3,6 +3,7 @@ from pydantic import ValidationError
 import warnings
 from src.main import fibonacci
 
+
 def test_fibonacci_sequence():
     # Test Fibonacci sequence generation
     assert fibonacci(1) == 2
@@ -12,6 +13,7 @@ def test_fibonacci_sequence():
     assert fibonacci(8) == 13
     assert fibonacci(13) == 21
     assert fibonacci(21) == 34
+
 
 def test_invalid_inputs():
     # Test invalid inputs
@@ -23,14 +25,13 @@ def test_invalid_inputs():
         fibonacci(-1)
     with pytest.raises(ValidationError):
         fibonacci(0)
-    for i in [4,6,7,9,12,14,15]:
+    for i in [4, 6, 7, 9, 12, 14, 15]:
         with warnings.catch_warnings(record=True) as w:
             # Cause the warning to be issued
             fibonacci(i)
-            
+
             # Check if any warning was issued
             assert len(w) == 1
-            
             # Check the message of the warning
             assert issubclass(w[0].category, Warning)
             assert "The provided number is not part of the fibonacci sequence" in str(w[0].message)
